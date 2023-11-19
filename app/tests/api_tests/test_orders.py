2567842077f7271f237698f2bb164641cddec25b
Stephan Matzke <stephan.matzke1995@googlemail.com>
2023-11-19 23:37:30 +0100

    clean formatting of tests, remove unnecessary parts

--- app/tests/api_tests/test_orders.py
+++ app/tests/api_tests/test_orders.py
@@ -1,23 +1,16 @@
-import pytest
-from fastapi import status, FastAPI
-from starlette.testclient import TestClient
-from Shop_API.app.tests.fixtures.fixtures import (db_fixture,
-                                                  order_data)
-app = FastAPI()
-test_client = TestClient(app)
+from fastapi import status
+from ..fixtures.fixtures import (test_client,
+                                 order_data,
+                                 temp_test_db)
+from ...src.schemas import OrderCreate
 
-
-@pytest.mark.usefixtures("db_fixture", "order_data")
-def test_create_transaction():
-    with db_fixture as db:
-        response = test_client.post("/order",
-                                    json=order_data,
-        )
-        assert response.status_code == status.HTTP_200_OK
-        response_data = response.json()
-        assert response_data["buyer_id"] == order_data["buyer_id"]
-        assert response_data["seller_id"] == order_data["seller_id"]
-        assert response_data["product_id"] == order_data["product_id"]
-        assert response_data["quantity"] == order_data["quantity"]
-        assert response_data["order_id"] == order_data["order_id"]
-        assert response_data["date"] == order_data["date"]
+def test_create_order(order_data: OrderCreate, temp_test_db):
+    response = test_client.post("/order", json=order_data)
+    response_data = response.json()
+    assert response.status_code == status.HTTP_200_OK
+    assert response_data["buyer_id"] == order_data["buyer_id"]
+    assert response_data["seller_id"] == order_data["seller_id"]
+    assert response_data["product_id"] == order_data["product_id"]
+    assert response_data["quantity"] == order_data["quantity"]
+    assert response_data["order_id"] == order_data["order_id"]
+    assert response_data["date"] == order_data["date"]
--- app/tests/api_tests/test_users.py
+++ app/tests/api_tests/test_users.py
@@ -1,23 +1,15 @@
-import pytest
-from fastapi import status, FastAPI
-from starlette.testclient import TestClient
-from Shop_API.app.tests.fixtures.fixtures import (db_fixture,
-                                                  users_create_data)
+from fastapi import status
+from ..fixtures.fixtures import (test_client,
+                                 users_create_data,
+                                 temp_test_db)
 
-
-app = FastAPI()
-test_client = TestClient(app)
-
-@pytest.mark.usefixtures("db_fixture", "users_create_data")
-def test_users_create_valid():
-    """Test creating a new user."""
-    with db_fixture as db:
-        response = test_client.post("/users/create", json=users_create_data)
-        response_data = response.json()
-        assert response.status_code == status.HTTP_200_OK
-        assert response_data["user_id"] == 6
-        assert response_data["email"] == users_create_data["email"]
-        assert response_data["points"] == users_create_data["points"]
-        assert response_data["shipping_address"] == users_create_data["shipping_address"]
+def test_users_create_valid(users_create_data, temp_test_db):
+    response = test_client.post("/users/create", json=users_create_data)
+    response_data = response.json()
+    assert response_data["user_id"] == 6
+    assert response.status_code == status.HTTP_200_OK
+    assert response_data["email"] == users_create_data["email"]
+    assert response_data["points"] == 10000
+    assert response_data["shipping_address"] == users_create_data["shipping_address"]
 
 
