0011dfe12eb29b62058af172eeea92b7de85539d
Stephan Matzke <stephan.matzke1995@googlemail.com>
2023-11-20 03:04:11 +0100

    only test for relevant item when creating order

--- app/tests/api_tests/test_orders.py
+++ app/tests/api_tests/test_orders.py
@@ -2,9 +2,8 @@ from fastapi import status
 from ..fixtures.fixtures import (test_client,
                                  order_data,
                                  temp_test_db)
-from ...src.schemas import OrderCreate
 
-def test_create_order(order_data: OrderCreate, temp_test_db):
+def test_create_order(order_data, temp_test_db):
     response = test_client.post("/order", json=order_data)
     response_data = response.json()
     assert response.status_code == status.HTTP_200_OK
@@ -12,5 +11,3 @@ def test_create_order(order_data: OrderCreate, temp_test_db):
     assert response_data["seller_id"] == order_data["seller_id"]
     assert response_data["product_id"] == order_data["product_id"]
     assert response_data["quantity"] == order_data["quantity"]
-    assert response_data["order_id"] == order_data["order_id"]
-    assert response_data["date"] == order_data["date"]
